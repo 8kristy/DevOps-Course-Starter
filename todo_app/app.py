467c1f6b3f24571b56cd92f17f61ed63fd6a2224
Kristina Soboleva <kristina.soboleva@softwire.com>
2024-11-12 12:15:59 +0000

    Disable log in for test

--- todo_app/app.py
+++ todo_app/app.py
@@ -14,6 +14,7 @@ def create_app():
     app.config.from_object(Config())
     app.cosmosDbService = CosmosDbService()
 
+    app.config['LOGIN_DISABLED'] = os.getenv('LOGIN_DISABLED') == 'True'
     login_manager = LoginManager()
     github_base_url = "https://github.com/login/oauth"
 
