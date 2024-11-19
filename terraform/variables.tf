256ef1eb57633ac8b98c54deda7c1edc6c1bc338
Kristina Soboleva <kristina.soboleva@softwire.com>
2024-11-19 18:29:30 +0000

    Use env variables in template

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,21 @@ variable "subscription_id" {
   sensitive = true
 }
 
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+
 variable "oauth_client_id" {
   type      = string
   sensitive = true
